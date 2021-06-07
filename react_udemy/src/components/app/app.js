132853fdf1f393e088bb62d5ed897a3123afb074
Igor <reshetnikovia@outlook.com>
2021-06-07 18:30:42 +0400

    added style imports

--- react_udemy/src/components/app/app.js
+++ react_udemy/src/components/app/app.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import './app.css';
 
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
 import PostStatusFilter from '../post-status-filter';
+import PostList from '../post-list';
+import PostAddForm from '../post-add-form';
 
 const App = () => {
     return (
         <div className="app">
             <AppHeader/>
-            <div className="searc-panel d-flex">
+            <div className="search-panel d-flex">
                 <SearchPanel/>
                 <PostStatusFilter/>
             </div> 
+            <PostList/>
+            <PostAddForm/>
         </div>
     
     )

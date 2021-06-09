d6d363a105b267a90033f109a8c560993f6cbf24
Igor <reshetnikovia@outlook.com>
2021-06-09 11:48:11 +0400

    add  class PostListItem

--- react_udemy/src/components/app/app.js
+++ react_udemy/src/components/app/app.js
@@ -8,6 +8,13 @@ import PostList from '../post-list';
 import PostAddForm from '../post-add-form';
 
 const App = () => {
+
+    const data = [
+        {label: 'Going to learn React', important: true, id: 1},
+        {label: 'That is do good', important: false, id: 2},
+        {label: 'I need a breack...', important: false, id: 3}
+    ]
+
     return (
         <div className="app">
             <AppHeader/>
@@ -15,7 +22,7 @@ const App = () => {
                 <SearchPanel/>
                 <PostStatusFilter/>
             </div> 
-            <PostList/>
+            <PostList posts={data}/>
             <PostAddForm/>
         </div>
     
